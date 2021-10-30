d8b0c6db7c2ccaece9ca27f89afd05d33b7938a7
John Cho <johncho108@gmail.com>
2021-10-30 17:45:42 -0400

    Prepped for Heroku

--- leetreview/settings.py
+++ leetreview/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--2kbj#)a%rxs1il21g!4sm!no7u#*gn=8n%kxdk-#yc$0+o6@d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['leetreview.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'leetreview.urls'
